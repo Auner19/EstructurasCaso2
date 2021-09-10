ab5d20d97b4d3d03211c8eefb57ed93e2148babd
Auner Olivas Artavia <61395923+Auner19@users.noreply.github.com>
2021-09-10 10:07:52 -0600

    EstructurasCaso2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "listasimple.h"
+#include "nodoEnteros.h"
+#include "nodoJugadores.h"
+#include "selectionsort.h"
+#include "insertionsort.h"
+
+using namespace std;
+
+
+int main() {
+
+cout << "hola" << endl;
+
+}
